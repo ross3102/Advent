fdd16fb1ebb74cdbbd4817eba344ba094d1f4942
Ross Newman <rzn3102@gmail.com>
2021-12-10 20:11:36 -0500

    2021 P8 rename

---
+++ advent2021/a08/a.py
@@ -0,0 +1,7 @@
+with open("in.txt", "r") as file:
+    total = 0
+    for line in file:
+        line = [side.split() for side in line.strip().split(" | ")]
+        out = line[1]
+        total += len([i for i in out if len(i) in [2,3,4,7]])
+    print(total)
