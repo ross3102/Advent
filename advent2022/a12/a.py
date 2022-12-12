12d393411dc5a5e63c3673076a19b37baa47c358
Ross Newman <rzn3102@gmail.com>
2022-12-12 00:14:42 -0500

    2022 p12

---
+++ advent2022/a12/a.py
@@ -0,0 +1,49 @@
+file = open("i", "r")
+
+def canMove(grid, f, t):
+    tcol, trow = t
+    fcol, frow = f
+    if 0 <= trow < len(grid) and 0 <= tcol < len(grid[trow]):
+        return grid[trow][tcol] <= grid[frow][fcol] + 1
+    return False
+
+def solve():
+    grid = []
+    locs = []
+    end = None
+    row = 0
+    for line in file:
+        line = line.strip()
+        gridrow = []
+        for col in range(len(line)):
+            c = line[col]
+            if c == "S":
+                gridrow.append(0)
+                locs.append((col, row))
+            elif c == "E":
+                gridrow.append(25)
+                end = (col, row)
+            else:
+                gridrow.append(ord(c) - ord('a'))
+        grid.append(gridrow)
+        row += 1
+    seen = set([locs[0]])
+    steps = 0
+    while True:
+        newlocs = []
+        for loc in locs:
+            lcol, lrow = loc
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                to = (lcol + dx, lrow + dy)
+                if to not in seen and canMove(grid, loc, to):
+                    newlocs.append(to)
+                    seen.add(to)
+                    if to == end:
+                        return steps + 1
+        locs = newlocs
+        steps += 1
+
+ans = solve()
+print(ans)
+
+file.close()
---
+++ advent2022/a12/b.py
@@ -0,0 +1,52 @@
+file = open("i", "r")
+
+
+def canMove(grid, f, t):
+    tcol, trow = t
+    fcol, frow = f
+    if 0 <= trow < len(grid) and 0 <= tcol < len(grid[trow]):
+        return grid[trow][tcol] <= grid[frow][fcol] + 1
+    return False
+
+
+def solve():
+    grid = []
+    locs = []
+    end = None
+    row = 0
+    for line in file:
+        line = line.strip()
+        gridrow = []
+        for col in range(len(line)):
+            c = line[col]
+            if c == "S" or c == "a":
+                gridrow.append(0)
+                locs.append((col, row))
+            elif c == "E":
+                gridrow.append(25)
+                end = (col, row)
+            else:
+                gridrow.append(ord(c) - ord('a'))
+        grid.append(gridrow)
+        row += 1
+    seen = set([locs[0]])
+    steps = 0
+    while True:
+        newlocs = []
+        for loc in locs:
+            lcol, lrow = loc
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                tcol, trow = lcol + dx, lrow + dy
+                if (tcol, trow) not in seen and canMove(grid, loc, (tcol, trow)):
+                    newlocs.append((tcol, trow))
+                    seen.add((tcol, trow))
+                    if (tcol, trow) == end:
+                        return steps + 1
+        locs = newlocs
+        steps += 1
+
+
+ans = solve()
+print(ans)
+
+file.close()
