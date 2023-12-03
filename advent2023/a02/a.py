3b138a21cbf76f087d23d69a2b4e3b351326f0b4
Ross Newman <rzn3102@gmail.com>
2023-12-03 00:55:15 -0500

    2023-2

--- advent2023/a02/a.py
+++ advent2023/a02/a.py
@@ -1,14 +1,31 @@
 from os import path
 file = open(path.dirname(__file__) + "/i", "r")
 
+amounts = {
+    "red": 12,
+    "green": 13,
+    "blue": 14    
+}
+
 def solve():
     ans = 0
     for line in file:
         line = line.strip()
-        
+        id = int(line.split(": ")[0].split()[1])
+        stuff = line.split(": ")[1]
+        rounds = stuff.split("; ")
+        good = True
+        for r in rounds:
+            things = r.split(", ")
+            for t in things:
+                n, c = t.split(" ")
+                if int(n) > amounts[c]:
+                    good = False
+
+        if good:
+            ans += id
     return ans
 
 ans = solve()
-print(ans)
 
-file.close()
+print(ans)
--- advent2023/a02/b.py
+++ advent2023/a02/b.py
@@ -5,10 +5,20 @@ def solve():
     ans = 0
     for line in file:
         line = line.strip()
-        
+        stuff = line.split(": ")[1]
+        rounds = stuff.split("; ")
+        maxes = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        for r in rounds:
+            things = r.split(", ")
+            for t in things:
+                n, c = t.split(" ")
+                maxes[c] = max(maxes[c], int(n))
+        ans += maxes["red"] * maxes["green"] * maxes["blue"]
     return ans
 
 ans = solve()
 print(ans)
-
-file.close()
